64a8991799b5f5d2879c2c1b9c9be624f8a67bd5
KLBMinhLong <nguyenminhlongcntt@gmail.com>
2026-01-30 15:32:45 +0700

    Initialize Spring Boot project with Maven Wrapper and basic structure

---
+++ src/main/java/com/example/MinhLongdemo/MinhLongdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.MinhLongdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhLongdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhLongdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MinhLongdemo/MinhLongdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MinhLongdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhLongdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
